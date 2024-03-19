7f1779f7698ff7ceeabe753a14e965fb15018017
JesusVA760 <tonio4340406@gmail.com>
2024-03-19 10:51:31 -0700

    progress

--- src/components/ExistingReply.jsx
+++ src/components/ExistingReply.jsx
@@ -20,19 +20,38 @@ const ExistingReply = () => {
   const secondComment = jsonData.comments[1];
   const comment = secondComment.replies.find((reply) => reply.id === commentId);
 
+  const priorCommentId = commentId - 1;
+  const priorCommentIndex = priorCommentId - 1;
+
+  let ThirdComment = null;
+  if (priorCommentIndex >= 0 && jsonData.comments[priorCommentIndex]) {
+    ThirdComment = jsonData.comments[priorCommentIndex];
+  }
+
   return (
     <div>
       <div className="existing-replies">
         {comment && (
           <div key={comment.id} className="existing-comment">
-            <div className="existing-button">
+            <div className="existing-comment-likes">
               <ButtonCard />
             </div>
-            <div className="existing-information">
-              <img src={Ramsesmiron} alt="profile" />
-              <title>{comment.user.username}</title>
-              <img src={ReplyArrow} alt="reply-arrow" />
-              <p>{comment.content}</p>
+            <div className="existing-content">
+              <div className="existing-content-header">
+                <img src={Ramsesmiron} alt="profile" />
+                <p>{comment.user.username}</p>
+                <div className="existing-reply">
+                  <img src={ReplyArrow} alt="reply-arrow" />
+                  <p>Reply</p>
+                </div>
+              </div>
+              <p className="at-sign-section">
+                <span className="at-sign">
+                  <span>@</span>
+                  {ThirdComment ? ThirdComment.user.username : ""}
+                </span>
+                <span className="reply-content"> {comment.content}</span>
+              </p>
             </div>
           </div>
         )}
